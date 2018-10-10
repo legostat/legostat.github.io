d21eeb6217ac09ede819bc42292c29b9d4595020
Oleg Kolisnyk <k4loki@gmail.com>
2018-10-10 18:20:09 +0300

    add biei

---
+++ biei/js/app.js
@@ -0,0 +1,66 @@
+"use strict";
+
+function Carousel(setting) {
+  var _this = this;
+
+  /* Scope privates methods and properties */
+  var privates = {};
+  /* Privates properties */
+
+  privates.setting = setting;
+  privates.sel = {
+    main: document.querySelector(privates.setting.main),
+    wrap: document.querySelector(privates.setting.wrap),
+    children: document.querySelector(privates.setting.wrap).children,
+    prev: document.querySelector(privates.setting.prev),
+    next: document.querySelector(privates.setting.next)
+  };
+  privates.opt = {
+    position: 0,
+    max_position: document.querySelector(privates.setting.wrap).children.length
+  }; // Control
+
+  if (privates.sel.prev !== null) {
+    privates.sel.prev.addEventListener("click", function () {
+      _this.prev_slide();
+    });
+  }
+
+  if (privates.sel.next !== null) {
+    privates.sel.next.addEventListener("click", function () {
+      _this.next_slide();
+    });
+  }
+  /* Public methods */
+  // Prev slide
+
+
+  this.prev_slide = function () {
+    --privates.opt.position;
+
+    if (privates.opt.position < 0) {
+      privates.sel.wrap.classList.add("s-notransition");
+      privates.opt.position = privates.opt.max_position - 1;
+    }
+
+    privates.sel.wrap.style["transform"] = "translateX(-".concat(privates.opt.position, "00%)");
+  }; // Next slide
+
+
+  this.next_slide = function () {
+    ++privates.opt.position;
+
+    if (privates.opt.position >= privates.opt.max_position) {
+      privates.opt.position = 0;
+    }
+
+    privates.sel.wrap.style["transform"] = "translateX(-".concat(privates.opt.position, "00%)");
+  };
+}
+
+new Carousel({
+  main: ".js-carousel",
+  wrap: ".js-carousel__wrap",
+  prev: ".js-carousel__prev",
+  next: ".js-carousel__next"
+});
